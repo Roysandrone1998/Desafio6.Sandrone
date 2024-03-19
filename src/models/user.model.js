5312822c97ea2a9f0fc99773b489aa66a209a9c3
Roysandrone1998 <raysandrone@gmail.com>
2024-03-19 16:56:13 -0300

    trabajando

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,34 +1,44 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
     first_name: {
-        type: String, 
+        type: String,
         required: true
     },
-
-    last_name : {
-        type: String, 
+    last_name: {
+        type: String,
         required: true
     },
-
-    email : {
-        type: String, 
+    email: {
+        type: String,
         required: true,
-        index: true, 
+        index: true,
         unique: true
-    }, 
-
+    },
     password: {
-        type: String, 
+        type: String,
         required: true
     },
-
-    age : {
-        type: Number, 
+    age: {
+        type: Number,
         required: true
     }
 });
 
-const UserModel = mongoose.model("user", userSchema);
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
 
+    try {
+        const hashedPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        return next(error);
+    }
+});
+
+const UserModel = mongoose.model("user", userSchema);
 module.exports = UserModel;
